6d75da3f8a2c206c2954993443fd85739a9cd584
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-09 18:13:23 -0300

    Added logger and renamed directory to vgg16_autoencoder

--- src/vgg16_autoencoder/__main__.py
+++ src/vgg16_autoencoder/__main__.py
@@ -1,10 +1,44 @@
 import torchvision
 import torch
+import argparse
 
-from style_transfer.dataset import VGG16DecoderImageDataset
-from style_transfer.model import VGG16Encoder, VGG16Decoder
-from style_transfer.loss import VGG16DecoderLossFunction
-from style_transfer.training import train_model, show_curves
+from vgg16_autoencoder.dataset import VGG16DecoderImageDataset
+from vgg16_autoencoder.model import VGG16Encoder, VGG16Decoder
+from vgg16_autoencoder.loss import VGG16DecoderLossFunction
+from vgg16_autoencoder.training import train_model, show_curves
+from vgg16_autoencoder.logger import LOGGER, setup_logger
+
+
+desc_str = """VGG16 training and evaluation code."""
+
+parser = argparse.ArgumentParser(description=desc_str)
+parser.add_argument(
+    "-d",
+    "--debug",
+    action="store_true",
+    help="Run in debug mode.",
+)
+parser.add_argument(
+    "-L",
+    "--log",
+    action="store_true",
+    help="Generate log files for the current run.",
+)
+parser.add_argument(
+    "-q",
+    "--quiet",
+    action="store_true",
+    help="Disable logging.",
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="Run in verbose mode.",
+)
+
+args = parser.parse_args()
+setup_logger(args.quiet, args.debug, args.verbose, args.log)
 
 
 LEARNING_RATE = 5e-4
@@ -15,6 +49,7 @@ NORM_MEAN = (0.485, 0.456, 0.406)
 NORM_STD = (0.229, 0.224, 0.225)
 
 # Define the transform for the data
+LOGGER.info("Setting up transforms for dataset.")
 transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Resize(256),
@@ -22,8 +57,12 @@ transform = torchvision.transforms.Compose([
     torchvision.transforms.Normalize(NORM_MEAN, NORM_STD),
 ])
 
+LOGGER.info("Creating encoder.")
 vgg_encoder = VGG16Encoder(use_gpu=USE_GPU)
 
+LOGGER.info("Creating datasets.")
+LOGGER.debug("Creating untransformed dataset.")
+LOGGER.debug("Creating untransformed dataset.")
 # Create the datasets
 untransformed_ds = VGG16DecoderImageDataset.from_dir(
     "data/test2017",
@@ -31,12 +70,14 @@ untransformed_ds = VGG16DecoderImageDataset.from_dir(
     transform=torchvision.transforms.ToTensor(),
     use_gpu=USE_GPU,
 )
+LOGGER.debug("Creating transformed dataset.")
 transformed_ds = VGG16DecoderImageDataset.from_dir(
     "data/test2017",
     encoder=vgg_encoder,
     transform=transform,
     use_gpu=USE_GPU,
 )
+LOGGER.debug("Creating reduced dataset.")
 reduced_ds = VGG16DecoderImageDataset.from_dir(
     "data/test2017",
     encoder=vgg_encoder,
