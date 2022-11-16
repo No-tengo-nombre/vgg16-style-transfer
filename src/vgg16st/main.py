b1480ca58c63b4c6effd5b58d8feaf3731e68b2e
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-16 12:14:11 -0300

    Started implementing the style transfer main function

--- src/vgg16st/main.py
+++ src/vgg16st/main.py
@@ -1,2 +1,14 @@
-def st_main():
+import re
+
+
+DEPTH_PATTERN = re.compile(r"([\+\-])(\d*)")
+
+
+def st_main(args):
     pass
+    # depths = args.depth
+    # match depths:
+    #     case [x]:
+    #         match DEPTH_PATTERN.search(x).groups():
+    #             case ("+", d):
+    #                 final_depths = 
