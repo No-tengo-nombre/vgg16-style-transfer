a154f509f5b2c66498926d0eec295aeb993da03c
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-16 10:28:24 -0300

    Implemented the whitening transform

--- src/vgg16st/transforms/whitening.py
+++ src/vgg16st/transforms/whitening.py
@@ -3,6 +3,9 @@ import torch
 from vgg16st.exceptions import MethodException
 
 
+EPSILON = 1e-30
+
+
 class Whitening:
     def __init__(self, method="paper") -> None:
         self.method = method
@@ -17,9 +20,21 @@ class Whitening:
 
 def __whitening_paper(content):
     # Center the image
+    channels, height, width = content.shape
     content_clone = content.clone()
-    c = content_clone - torch.mean(content_clone)
+    c = content_clone - torch.mean(content_clone, (1, 2)).reshape(1, 1, -1).T
 
     # Calculate eigenvalues and eigenvectors of covariance matrix
-    cov_mat = (c @ c.T) / (c.shape[1] * c.shape[2] - 1)
+    cov_mat = (c.reshape(channels, -1) @ c.reshape(channels, -1).T) / (height * width - 1)
     vals, vecs = torch.linalg.eig(cov_mat)
+    vals = vals.real
+    vecs = vecs.real
+
+    # We remove negative values and zeros
+    reduced_dimension = (vals > EPSILON).sum()
+    vals = vals[:reduced_dimension]
+    vecs = vecs[:, :reduced_dimension]
+
+    # Apply the whitening transformation
+    whitened = vecs @ torch.diag(vals) @ vecs.T @ c
+    return whitened
