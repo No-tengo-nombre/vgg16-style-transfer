675e141279b8dab6e5a9c43233b64299ab82614a
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-16 11:51:05 -0300

    Added an import to the transforms subpackage

--- src/vgg16st/transforms/__init__.py
+++ src/vgg16st/transforms/__init__.py
@@ -1,2 +1,3 @@
-from vgg16st.transforms.whitening import Whitening
 from vgg16st.transforms.coloring import Coloring
+from vgg16st.transforms.wct import WhiteningColoring
+from vgg16st.transforms.whitening import Whitening
