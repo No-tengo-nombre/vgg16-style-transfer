69a14fa5836a3e627918e25b27a9bb08a11e3e71
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-16 23:11:42 -0300

    Made an attempt to fix the implementation

--- src/vgg16st/transforms/coloring.py
+++ src/vgg16st/transforms/coloring.py
@@ -23,11 +23,15 @@ def __coloring_paper(content, parameter_content=None):
     else:
         c, _, vals, vecs = parameters_from_image(parameter_content)
 
+    # Resize the matrix
+    c_shape = c.shape
+    c_mat = c.reshape(c_shape[0], -1)
+
     # We remove negative values and zeros
     reduced_dimension = (vals > EPSILON).sum()
     vals = vals[:reduced_dimension]
     vecs = vecs[:, :reduced_dimension]
 
     # Apply the coloring transformation
-    colored = vecs @ torch.diag(vals).pow(0.5) @ vecs.T @ c
-    return colored
+    colored = vecs @ torch.diag(vals).pow(0.5) @ vecs.T @ c_mat
+    return colored.reshape(*c_shape)
--- src/vgg16st/transforms/whitening.py
+++ src/vgg16st/transforms/whitening.py
@@ -2,6 +2,7 @@ import torch
 
 from vgg16st.exceptions import MethodException
 from vgg16st.functions import parameters_from_image
+from vgg16autoencoder.logger import LOGGER
 from vgg16autoencoder import EPSILON
 
 
@@ -18,16 +19,23 @@ class Whitening:
 
 
 def __whitening_paper(content, parameter_content=None):
+    LOGGER.info("Calculating whitening with paper method.")
     if parameter_content is None:
         c, _, vals, vecs = parameters_from_image(content)
     else:
         c, _, vals, vecs = parameters_from_image(parameter_content)
 
+    # Resize the matrix
+    c_shape = c.shape
+    c_mat = c.reshape(c_shape[0], -1)
+
     # We remove negative values and zeros
     reduced_dimension = (vals > EPSILON).sum()
     vals = vals[:reduced_dimension]
     vecs = vecs[:, :reduced_dimension]
+    vals_mat = torch.diag(vals).pow(-0.5)
 
     # Apply the whitening transformation
-    whitened = vecs @ torch.diag(vals).pow(-0.5) @ vecs.T @ c
-    return whitened
+    LOGGER.info(f"Whitening shapes {vecs.shape}, {vals_mat.shape}, {c_mat.shape}")
+    whitened = vecs @ vals_mat @ vecs.T @ c_mat
+    return whitened.reshape(*c_shape)
