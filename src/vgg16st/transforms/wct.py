20d5c6e5a0e91b538a3a0c126566f6b560f4e960
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-18 17:51:23 -0300

    Added logs to the wct

--- src/vgg16st/transforms/wct.py
+++ src/vgg16st/transforms/wct.py
@@ -1,3 +1,4 @@
+from vgg16autoencoder.logger import LOGGER
 from vgg16st.transforms.whitening import Whitening
 from vgg16st.transforms.coloring import Coloring
 from vgg16st.functions import parameters_from_image
@@ -19,6 +20,7 @@ class WhiteningColoring:
 
     def __call__(self, content, style, alpha=None):
         # Determine the blending parameter
+        LOGGER.info("Determining alpha.")
         if alpha is None:
             blending = self.alpha
         else:
@@ -26,11 +28,16 @@ class WhiteningColoring:
 
         # Apply the wct
         _, style_mean, *_ = parameters_from_image(style)
+        LOGGER.info("Applying whitening.")
         whitened_content = self.whitening(content)
+        LOGGER.info("Applying coloring.")
         colored_content = self.coloring(whitened_content, style)
 
         # Readjust by the style means and blend
+        LOGGER.info("Readjusting stylized image.")
         stylized_image = colored_content + style_mean.reshape(1, 1, -1).T
+
+        LOGGER.info(f"Blending. Stylized shape: {stylized_image.shape}, content shape: {content.shape}.")
         blended_image = blending * stylized_image + (1 - blending) * content
 
         return blended_image
