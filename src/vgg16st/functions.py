b5bccf2f0425dd03442c17883c76a1cc82bc8f3f
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-18 18:54:23 -0300

    Removed the use of deprecated functionality

--- src/vgg16st/functions.py
+++ src/vgg16st/functions.py
@@ -5,8 +5,10 @@ def parameters_from_image(image):
     # Center the image
     channels, height, width = image.shape
     content_clone = image.clone()
+
+    # Calculate the mean for every channel
     mean_val = torch.mean(content_clone, (1, 2))
-    c = content_clone - mean_val.reshape(1, 1, -1).T
+    c = content_clone - mean_val.reshape(-1, 1, 1)
 
     # Calculate eigenvalues and eigenvectors of covariance matrix
     cov_mat = (c.reshape(channels, -1) @ c.reshape(channels, -1).T) / (height * width - 1)
