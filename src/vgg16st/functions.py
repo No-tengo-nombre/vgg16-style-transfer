74a1b85de8d2a32a5dbcdc01396f1f62f2f1744e
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-18 19:03:18 -0300

    Separated into a centering function and one to extract the eigenvalues from the covariance matrix

--- src/vgg16st/functions.py
+++ src/vgg16st/functions.py
@@ -1,19 +1,22 @@
 import torch
 
 
-def parameters_from_image(image):
-    # Center the image
-    channels, height, width = image.shape
+def center_tensor(image):
     content_clone = image.clone()
 
     # Calculate the mean for every channel
     mean_val = torch.mean(content_clone, (1, 2))
     c = content_clone - mean_val.reshape(-1, 1, 1)
 
+    return c, mean_val
+
+def cov_eigvals(image):
+    channels, height, width = image.shape
+
     # Calculate eigenvalues and eigenvectors of covariance matrix
-    cov_mat = (c.reshape(channels, -1) @ c.reshape(channels, -1).T) / (height * width - 1)
+    cov_mat = (image.reshape(channels, -1) @ image.reshape(channels, -1).T) / (height * width - 1)
     vals, vecs = torch.linalg.eig(cov_mat)
     vals = vals.real
     vecs = vecs.real
 
-    return c, mean_val, vals, vecs
+    return vals, vecs
