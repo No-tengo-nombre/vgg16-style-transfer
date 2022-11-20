e7c2db7e3ea700c39d5fc6bd0a4d1c8237a4fcb1
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-20 16:46:10 -0300

    Added a log when saving the best model

--- src/vgg16autoencoder/training.py
+++ src/vgg16autoencoder/training.py
@@ -152,6 +152,7 @@ def train_model(model, train_dataset, val_dataset, epochs, criterion,
                 with open(os.path.join(PATH_TO_WEIGHTS, f"best{model.depth}.toml"), "r") as f:
                     best_model = toml.load(f)
                 if float(best_model["final_losses"]["validation"]) > val_loss:
+                    LOGGER.warning("Saving new best model.")
                     model.save_model(os.path.join(PATH_TO_WEIGHTS, f"best{model.depth}.pt"), data_dict)
             except FileNotFoundError:
                 LOGGER.warning("Best model not found (maybe it was deleted?). Saving it anyways.")
