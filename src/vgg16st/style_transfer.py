7b559c870a5b4cb1525ca4fc78983530ecacfc63
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-21 20:54:43 -0300

    Made encoders and decoders initialize on every depth, and added option to use the vendor wct function

--- src/vgg16st/style_transfer.py
+++ src/vgg16st/style_transfer.py
@@ -1,4 +1,3 @@
-import torch
 import torchvision
 import os
 
@@ -6,6 +5,7 @@ from vgg16autoencoder.model import VGG16Decoder, VGG16Encoder
 from vgg16autoencoder import PATH_TO_WEIGHTS
 from vgg16common import LOGGER, NORM_MEAN, NORM_STD
 from vgg16st.transforms import WhiteningColoring
+from vgg16st.vendor import wct
 
 
 def transfer_style(content, style, depths=(1, 2, 3, 4, 5), use_gpu=False, alpha=1, method="paper"):
@@ -14,24 +14,7 @@ def transfer_style(content, style, depths=(1, 2, 3, 4, 5), use_gpu=False, alpha=
         -NORM_MEAN / NORM_STD,
         1 / NORM_STD
     )
-    wct = WhiteningColoring(alpha, method=method)
-
-    # Create the encoders and decoders
-    encoders = []
-    decoders = []
-    LOGGER.info("Creating encoders and decoders")
-    for d in depths:
-        encoders.append(VGG16Encoder(depth=d, use_gpu=use_gpu))
-        decoders.append(
-            VGG16Decoder.from_state_dict(
-                depth=d,
-                path=os.path.join(
-                    PATH_TO_WEIGHTS,
-                    f"best{d}.pt",
-                ),
-                use_gpu=use_gpu,
-            )
-        )
+    # wct = WhiteningColoring(alpha, method=method)
 
     # Normalizing images
     content_img = normalization(content)
@@ -45,12 +28,23 @@ def transfer_style(content, style, depths=(1, 2, 3, 4, 5), use_gpu=False, alpha=
 
     # Apply each stylization
     LOGGER.info("Applying stylization.")
-    for encoder, decoder in zip(encoders, decoders):
-        LOGGER.info(f"Stylization level {encoder.depth}")
-        stylized_feats = wct(encoder(content_img), encoder(style_img))
+    for d in depths:
+        encoder = VGG16Encoder(depth=d, use_gpu=use_gpu)
+        decoder = VGG16Decoder.from_state_dict(
+            depth=d,
+            path=os.path.join(
+                PATH_TO_WEIGHTS,
+                f"best{d}.pt",
+            ),
+            use_gpu=use_gpu,
+        )
+        LOGGER.info(f"Stylization level {d}")
+        # stylized_feats = wct(encoder(content_img), encoder(style_img))
+        stylized_feats = wct(alpha, encoder(content_img).detach(), encoder(style_img).detach())
 
         # Apply the decoder
         LOGGER.info("Decoding styled features.")
-        content_img = decoder(stylized_feats.reshape(1, *stylized_feats.shape))[0]
+        # content_img = decoder(stylized_feats.reshape(1, *stylized_feats.shape))[0]
+        content_img = decoder(stylized_feats)[0]
 
     return inverse_normalization(content_img).detach().cpu()
