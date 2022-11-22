325b36ec42693a4ceda14d4b8af503bee025a10e
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-22 18:52:03 -0300

    Trying some stuff to learn how to use kivy

--- src/vgg16kv/app/app.py
+++ src/vgg16kv/app/app.py
@@ -1,11 +1,22 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import StringProperty, ListProperty, NumericProperty
 
 
-class StyleTransferWidget(Widget):
+class StyleTransferImageRow(Widget):
+    images = ListProperty([])
+
+
+class StyleTransferImage(Widget):
+    image_file = StringProperty("")
+    size_x = NumericProperty(100)
+    size_y = NumericProperty(100)
+
+
+class StyleTransferBase(Widget):
     pass
 
 
 class StyleTransferApp(App):
     def build(self):
-        return StyleTransferWidget()
+        return StyleTransferBase()
