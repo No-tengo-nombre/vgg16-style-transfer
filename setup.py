a90cc384be6cb4ba676b9e0e24493d894cbde530
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-25 22:26:06 -0300

    Tried implementing setuptools_rust into the package

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        rust_extensions=[
+            RustExtension("vgg16st.vgg16st", binding=Binding.PyO3),
+        ]
+    )
