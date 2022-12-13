d789136cd2a396feadc216c452ffeeb13d610425
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-13 18:17:12 -0300

    Added a VGG19 decoder

--- src/vgg16autoencoder/model/decoder.py
+++ src/vgg16autoencoder/model/decoder.py
@@ -77,3 +77,82 @@ class VGG16Decoder(nn.Module):
             with open(f"{'.'.join(path.split('.')[:-1])}.toml", "w") as f:
                 LOGGER.info(f"Saving data in {'.'.join(path.split('.')[:-1])}.toml.")
                 toml.dump(data_dict, f)
+
+
+class VGG19Decoder(nn.Module):
+    def __init__(self, depth=5, use_gpu=False):
+        LOGGER.info(f"Initializing VGG19 decoder (depth: {depth}, use_gpu: {use_gpu}).")
+        self.depth = depth
+        self.use_gpu = use_gpu
+        super().__init__()
+        all_layers = nn.Sequential(
+            # Depth 5
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(512,512,(3, 3)),
+            nn.ReLU(),
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(512,512,(3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(512,512,(3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(512,512,(3, 3)),
+            nn.ReLU(),
+            # Depth 4
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(512,256,(3, 3)),
+            nn.ReLU(),
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(256,256,(3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(256,256,(3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(256,256,(3, 3)),
+            nn.ReLU(),
+            # Depth 3
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(256,128,(3, 3)),
+            nn.ReLU(),
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(128,128,(3, 3)),
+            nn.ReLU(),
+            # Depth 2
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(128,64,(3, 3)),
+            nn.ReLU(),
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(64,64,(3, 3)),
+            nn.ReLU(),
+            # Depth 1
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Conv2d(64,3,(3, 3)),
+        )
+        if use_gpu:
+            all_layers = all_layers.to("cuda")
+
+        # Hardcoded indices for each depth
+        indices = {
+            1: 40,
+            2: 33,
+            3: 26,
+            4: 13,
+            5: 0,
+        }
+        self.model = all_layers[indices[depth]:]
+        LOGGER.info(f"Decoder layers\n{self.model}.")
+
+    @classmethod
+    def from_state_dict(cls, *args, path, **kwargs):
+        model = cls(*args, **kwargs)
+        model.load_state_dict(torch.load(path))
+        return model
+
+    def forward(self, x):
+        return self.model(x)
